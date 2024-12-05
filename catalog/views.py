51b4e20bb779ea5a4c22ed7b9e3895377e43ca30
Basant Morsi <basant179@gmail.com>
2024-12-05 11:24:30 +0100

    created generic list for book-list, book-detail, author-list, author-details

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
 
 def index(request):
     """view function for homepage of site"""
@@ -8,18 +10,33 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact ='a').count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
     # The 'all()' is implied by default
     num_authors = Author.objects.count()
 
     context = {
-        'num_books' : num_books,
-        'num_instances' : num_instances,
-        'num_instances_available' : num_instances_available,
-        'num_authors' : num_authors
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 3
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
