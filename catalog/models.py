7e4412221939a12e2d4c064418a26aab62c7e17f
Basant Morsi <basant179@gmail.com>
2024-12-06 10:40:57 +0100

    user authentications and permissions

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
+from django.urls import reverse  # Used in get_absolute_url() to get URL for specified ID
 
-from django.db.models import UniqueConstraint # Constrains fields to unique values
-from django.db.models.functions import Lower # Returns lower cased value of field
+from django.db.models import UniqueConstraint  # Constrains fields to unique values
+from django.db.models.functions import Lower  # Returns lower cased value of field
 import uuid  # Required for unique book instances
+from django.contrib.auth.models import User
+from datetime import date
 
 
 class Genre(models.Model):
@@ -27,10 +29,9 @@ class Genre(models.Model):
             UniqueConstraint(
                 Lower('name'),
                 name='genre_name_case_insensitive_unique',
-                violation_error_message = "Genre already exists (case insensitive match)"
+                violation_error_message="Genre already exists (case insensitive match)"
             ),
         ]
-
 
 
 class Book(models.Model):
@@ -42,9 +43,9 @@ class Book(models.Model):
 
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN', max_length=13,
-                                    unique=True,
-                                    help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
-                                              '">ISBN number</a>')
+                            unique=True,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>')
 
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
@@ -55,8 +56,8 @@ class Book(models.Model):
         return self.title
 
     def get_absolute_url(self):
-                """Returns the URL to access a detail record for this book."""
-                return reverse('book-detail', args=[str(self.id)])
+        """Returns the URL to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
 
     def display_genre(self):
         """Create a string for the Genre. This is required to display genre in Admin."""
@@ -65,56 +66,60 @@ class Book(models.Model):
     display_genre.short_description = 'Genre'
 
 
-
 class BookInstance(models.Model):
-        """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
-        id = models.UUIDField(primary_key=True, default=uuid.uuid4,
-                                      help_text="Unique ID for this particular book across whole library")
-        book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
-        imprint = models.CharField(max_length=200)
-        due_back = models.DateField(null=True, blank=True)
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
 
-        LOAN_STATUS = (
-                    ('m', 'Maintenance'),
-                    ('o', 'On loan'),
-                    ('a', 'Available'),
-                    ('r', 'Reserved'),
-                )
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
 
-        status = models.CharField(
-                    max_length=1,
-                    choices=LOAN_STATUS,
-                    blank=True,
-                    default='m',
-                    help_text='Book availability',
-                )
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
-        class Meta:
-                    ordering = ['due_back']
+    class Meta:
+        ordering = ['due_back']
 
-        def __str__(self):
-                    """String for representing the Model object."""
-                    return f'{self.id} ({self.book.title})'
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id} ({self.book.title})'
 
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
 
 class Author(models.Model):
-            """Model representing an author."""
-            first_name = models.CharField(max_length=100)
-            last_name = models.CharField(max_length=100)
-            date_of_birth = models.DateField(null=True, blank=True)
-            date_of_death = models.DateField('Died', null=True, blank=True)
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
 
-            class Meta:
-                        ordering = ['last_name', 'first_name']
+    class Meta:
+        ordering = ['last_name', 'first_name']
 
-            def get_absolute_url(self):
-                        """Returns the URL to access a particular author instance."""
-                        return reverse('author-detail', args=[str(self.id)])
+    def get_absolute_url(self):
+        """Returns the URL to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
 
-            def __str__(self):
-                        """String for representing the Model object."""
-                        return f'{self.last_name}, {self.first_name}'
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
 
 
 class Language(models.Model):
@@ -136,6 +141,6 @@ class Language(models.Model):
             UniqueConstraint(
                 Lower('name'),
                 name='language_name_case_insensitive_unique',
-                violation_error_message = "Language already exists (case insensitive match)"
+                violation_error_message="Language already exists (case insensitive match)"
             ),
         ]
